998c84758353a8ec20c54ecaed2987bb0c687719
Rorspike <Rorspike@users.noreply.github.com>
2017-07-27 23:15:50 +0800

    Update srez_train.py

--- srez_train.py
+++ srez_train.py
@@ -74,41 +74,42 @@ def train_model(train_data):
 
     # Cache test features and labels (they are small)
     test_feature, test_label = td.sess.run([td.test_features, td.test_labels])
+    
+    for ii in range(10):
+        while not done:
+            batch += 1
+            gene_loss = disc_real_loss = disc_fake_loss = -1.234
 
-    while not done:
-        batch += 1
-        gene_loss = disc_real_loss = disc_fake_loss = -1.234
+            feed_dict = {td.learning_rate : lrval}
 
-        feed_dict = {td.learning_rate : lrval}
+            ops = [td.gene_minimize, td.disc_minimize, td.gene_loss, td.disc_real_loss, td.disc_fake_loss]
+            _, _, gene_loss, disc_real_loss, disc_fake_loss = td.sess.run(ops, feed_dict=feed_dict)
 
-        ops = [td.gene_minimize, td.disc_minimize, td.gene_loss, td.disc_real_loss, td.disc_fake_loss]
-        _, _, gene_loss, disc_real_loss, disc_fake_loss = td.sess.run(ops, feed_dict=feed_dict)
-        
-        if batch % 10 == 0:
-            # Show we are alive
-            elapsed = int(time.time() - start_time)/60
-            print('Progress[%3d%%], ETA[%4dm], Batch [%4d], G_Loss[%3.3f], D_Real_Loss[%3.3f], D_Fake_Loss[%3.3f]' %
-                  (int(100*elapsed/FLAGS.train_time), FLAGS.train_time - elapsed,
-                   batch, gene_loss, disc_real_loss, disc_fake_loss))
+            if batch % 10 == 0:
+                # Show we are alive
+                elapsed = int(time.time() - start_time)/60
+                print('Epoch[%2d / 10], Progress[%3d%%], ETA[%4dm], Batch [%4d], G_Loss[%3.3f], D_Real_Loss[%3.3f], D_Fake_Loss[%3.3f]' %
+                      (ii, int(100*elapsed/FLAGS.train_time), FLAGS.train_time - elapsed,
+                       batch, gene_loss, disc_real_loss, disc_fake_loss))
 
-            # Finished?            
-            current_progress = elapsed / FLAGS.train_time
-            if current_progress >= 1.0:
-                done = True
-            
-            # Update learning rate
-            if batch % FLAGS.learning_rate_half_life == 0:
-                lrval *= .5
+                # Finished?            
+                current_progress = elapsed / FLAGS.train_time
+                if current_progress >= 1.0:
+                    done = True
 
-        if batch % FLAGS.summary_period == 0:
-            # Show progress with test features
-            feed_dict = {td.gene_minput: test_feature}
-            gene_output = td.sess.run(td.gene_moutput, feed_dict=feed_dict)
-            _summarize_progress(td, test_feature, test_label, gene_output, batch, 'out')
-            
-        if batch % FLAGS.checkpoint_period == 0:
-            # Save checkpoint
-            _save_checkpoint(td, batch)
+                # Update learning rate
+                if batch % FLAGS.learning_rate_half_life == 0:
+                    lrval *= .5
 
-    _save_checkpoint(td, batch)
+            if batch % FLAGS.summary_period == 0:
+                # Show progress with test features
+                feed_dict = {td.gene_minput: test_feature}
+                gene_output = td.sess.run(td.gene_moutput, feed_dict=feed_dict)
+                _summarize_progress(td, test_feature, test_label, gene_output, batch, 'out')
+
+            if batch % FLAGS.checkpoint_period == 0:
+                # Save checkpoint
+                _save_checkpoint(td, batch)
+
+        _save_checkpoint(td, batch)
     print('Finished training!')
