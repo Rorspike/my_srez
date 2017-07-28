60c79e81d64f07ac074e36ca8dfbb429bbbe924b
Rorspike <Rorspike@users.noreply.github.com>
2017-07-28 22:11:35 +0800

    Update srez_demo.py

--- srez_demo.py
+++ srez_demo.py
@@ -7,22 +7,36 @@ import tensorflow as tf
 
 FLAGS = tf.app.flags.FLAGS
 
-def demo1(sess):
-    """Demo based on images dumped during training"""
+def demo1(train_data):
+    
+    td = train_data
+    
+    feature, label = td.sess.run([td.features, td.labels])
 
-    # Get images that were dumped during training
-    filenames = tf.gfile.ListDirectory(FLAGS.train_dir)
-    filenames = sorted(filenames)
-    filenames = [os.path.join(FLAGS.train_dir, f) for f in filenames if f[-4:]=='.png']
+    feed_dict = {td.gene_minput: feature}
+    gene_output = td.sess.run(td.gene_moutput, feed_dict=feed_dict)
+    #_summarize_progress(td, test_feature, test_label, gene_output, batch, 'out')
+    clipped = tf.maximum(tf.minimum(gene_output, 1.0), 0.0)
+    filename = 'demo_output.png'
+    filename = os.path.join(FLAGS.train_dir, filename)
+    scipy.misc.toimage(clipped, cmin=0., cmax=1.).save(filename)
+    print("    Saved %s" % (filename,))
+            
+            
+#     """Demo based on images dumped during training"""
+#     # Get images that were dumped during training
+#     filenames = tf.gfile.ListDirectory(FLAGS.train_dir)
+#     filenames = sorted(filenames)
+#     filenames = [os.path.join(FLAGS.train_dir, f) for f in filenames if f[-4:]=='.png']
 
-    assert len(filenames) >= 1
+#     assert len(filenames) >= 1
 
-    fps        = 30
+#     fps        = 30
 
-    # Create video file from PNGs
-    print("Producing video file...")
-    filename  = os.path.join(FLAGS.train_dir, 'demo1.mp4')
-    clip      = mpe.ImageSequenceClip(filenames, fps=fps)
-    clip.write_videofile(filename)
-    print("Done!")
+#     # Create video file from PNGs
+#     print("Producing video file...")
+#     filename  = os.path.join(FLAGS.train_dir, 'demo1.mp4')
+#     clip      = mpe.ImageSequenceClip(filenames, fps=fps)
+#     clip.write_videofile(filename)
+#     print("Done!")
     
