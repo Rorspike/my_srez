e63bc4b3973a510107493eb9ed6c49f12aa2dd48
Rorspike <Rorspike@users.noreply.github.com>
2017-07-28 23:28:14 +0800

    Update srez_demo.py

--- srez_demo.py
+++ srez_demo.py
@@ -12,11 +12,27 @@ def demo1(train_data):
     td = train_data
     
     feature, label = td.sess.run([td.features, td.labels])
-
+    
     feed_dict = {td.gene_minput: feature}
     gene_output = td.sess.run(td.gene_moutput, feed_dict=feed_dict)
-    #_summarize_progress(td, test_feature, test_label, gene_output, batch, 'out')
+    
+    size = [label.shape[1], label.shape[2]]
+
+    nearest = tf.image.resize_nearest_neighbor(feature, size)
+    nearest = tf.maximum(tf.minimum(nearest, 1.0), 0.0)
+
+    bicubic = tf.image.resize_bicubic(feature, size)
+    bicubic = tf.maximum(tf.minimum(bicubic, 1.0), 0.0)
+
     clipped = tf.maximum(tf.minimum(gene_output, 1.0), 0.0)
+    
+    image   = tf.concat(2, [nearest, bicubic, clipped, label])
+
+    image = image[0:max_samples,:,:,:]
+    image = td.sess.run(image)
+   
+    #_summarize_progress(td, test_feature, test_label, gene_output, batch, 'out')
+    
     filename = 'demo_output.png'
     filename = os.path.join(FLAGS.train_dir, filename)
     scipy.misc.toimage(clipped, cmin=0., cmax=1.).save(filename)
